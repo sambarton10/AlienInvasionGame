29a462534475dad30dccddf3c7c4569ca7e8003c
sambarton10 <69605114+sambarton10@users.noreply.github.com>
2020-11-16 20:12:36 -0600

    Added fullscreen

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,8 +13,10 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
         
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+        
         pygame.display.set_caption("Alien Invasion")
         
         self.ship = Ship(self)
@@ -38,9 +40,10 @@ class AlienInvasion:
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RIGHT:
                         self.ship.moving_right = True
-                    if event.key == pygame.K_LEFT:
+                    elif event.key == pygame.K_LEFT:
                         self.ship.moving_left = True
-                           
+                    elif event.key == pygame.K_q:
+                        sys.exit()
                 elif event.type == pygame.KEYUP:
                     if event.key ==pygame.K_RIGHT:
                         self.ship.moving_right = False
@@ -48,7 +51,7 @@ class AlienInvasion:
                         self.ship.moving_left = False
                         
                         self.ship.rect.x +=1
-         
+
             # Redraw the screem during each pass through the loop.
     def _Update_screen(self):       
             """Update images on the screen, and flip to the new screen."""
